3c2ac736987c5272e19401f024fc7cbed6db3af1
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-12 02:02:03 +0530

    Added function to get post by postId

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,13 +16,26 @@ const getPosts = async (req, res) => {
   res.send(posts);
 };
 
+const getPostById = async (req, res) => {
+  const { postId } = req.params;
+  const post = await prisma.post.findUnique({
+    where: {
+      id: parseInt(postId),
+    },
+  });
+
+  if (!post) return res.sendStatus(500);
+
+  res.send(post);
+};
+
 const createPost = async (req, res) => {
   const { content, parentPostId = null } = req.body;
-  const post = await prisma.message.create({
+  const post = await prisma.post.create({
     data: {
       content: content,
-      author_id: req.user.id,
-      parentPostId: parentPostId
+      authorId: req.user.id,
+      parentPostId: parseInt(parentPostId),
     },
   });
 
@@ -35,7 +48,7 @@ const createPost = async (req, res) => {
 
 const deletePost = async (req, res) => {
   const { postId } = res.params;
-  const post = await prisma.message.delete({
+  const post = await prisma.post.delete({
     where: {
       id: parseInt(postId),
       author_id: parseInt(req.user.id),
@@ -48,8 +61,7 @@ const deletePost = async (req, res) => {
 
 export default {
   getPosts,
+  getPostById,
   createPost,
   deletePost,
 };
-
-

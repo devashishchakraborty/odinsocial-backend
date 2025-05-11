fe7650ac564e5f8017deeec9f12987f0e7eae13b
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-12 01:54:07 +0530

    Added basic create, read, delete functionality for posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,3 +16,40 @@ const getPosts = async (req, res) => {
   res.send(posts);
 };
 
+const createPost = async (req, res) => {
+  const { content, parentPostId = null } = req.body;
+  const post = await prisma.message.create({
+    data: {
+      content: content,
+      author_id: req.user.id,
+      parentPostId: parentPostId
+    },
+  });
+
+  if (!post) {
+    return res.sendStatus(500);
+  }
+
+  res.send(post);
+};
+
+const deletePost = async (req, res) => {
+  const { postId } = res.params;
+  const post = await prisma.message.delete({
+    where: {
+      id: parseInt(postId),
+      author_id: parseInt(req.user.id),
+    },
+  });
+
+  if (!post) return res.sendStatus(500);
+  res.send(post);
+};
+
+export default {
+  getPosts,
+  createPost,
+  deletePost,
+};
+
+

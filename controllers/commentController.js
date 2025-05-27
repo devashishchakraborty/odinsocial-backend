a7d5655db6a25b32b27cc79f959de5580e32b8e0
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-28 04:01:48 +0530

    Added CommentController with routes for serving comment and replies to a post

---
+++ controllers/commentController.js
@@ -0,0 +1,35 @@
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+const getCommentsByPostId = async (req, res) => {
+  const { postId } = res.params;
+  const comments = await prisma.comment.findMany({
+    where: {
+      postId: parseInt(postId),
+    },
+  });
+
+  if (!comments) {
+    return res.sendStatus(500);
+  }
+
+  res.send(comments);
+};
+
+const getRepliesByCommentId = async (req, res) => {
+  const { commentId } = res.params;
+  const replies = await prisma.reply.findMany({
+    where: {
+      commentId: parseInt(commentId),
+    },
+  });
+
+  if (!replies) {
+    return res.sendStatus(500);
+  }
+
+  res.send(replies);
+};
+
+export default { getCommentsByPostId, getRepliesByCommentId };
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import postController from "../controllers/postController.js";
-
+import commentController from "../controllers/commentController.js";
 const postRouter = Router();
 
 postRouter.get("/", postController.getPosts);
-postRouter.get("/:postId", postController.getPostById)
+postRouter.get("/:postId", postController.getPostById);
+postRouter.get("/:postId/comments", commentController.getCommentsByPostId);
+postRouter.get(
+  "/:postId/comments/:commentId/replies",
+  commentController.getRepliesByCommentId
+);
 postRouter.post("/", postController.createPost);
-postRouter.put("/:postId", postController.updatePost)
-postRouter.delete("/delete/:postId", postController.deletePost)
+postRouter.put("/:postId", postController.updatePost);
+postRouter.delete("/delete/:postId", postController.deletePost);
 
 export default postRouter;

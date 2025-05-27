43af64515dd37d0d78f0177e1dd6f4493249ba0e
Devashish Chakraborty <devashishchakra@gmail.com>
2025-05-28 04:01:10 +0530

    Added related table attribute with the post response

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,16 +3,24 @@ import { PrismaClient } from "../generated/prisma/index.js";
 const prisma = new PrismaClient();
 
 const getPosts = async (req, res) => {
-  const { following, userId } = req.query;
+  const { following, userId, showBookmarks } = req.query;
   const filter = {};
 
-  if (userId) filter.authorId = parseInt(userId);
+  if (userId) filter.authorId = parseInt(userId); // Posts by User Id
   else if (following === "true") {
+    // To show posts by people the user is following
     filter.author = {
       followers: {
         some: {
           followerId: req.user.id,
         },
+      },
+    };
+  } else if (showBookmarks === "true") {
+    // To show user's bookmarked posts
+    filter.bookmarkedBy = {
+      some: {
+        id: req.user.id,
       },
     };
   }
@@ -63,21 +71,21 @@ const getPosts = async (req, res) => {
 
 const updatePost = async (req, res) => {
   const { postId } = req.params;
-  const { userId, isLiked, isBookmarked } = req.body;
+  const { isLiked, isBookmarked } = req.body;
   let action = {};
 
   if (isLiked) {
     action.likedBy =
       isLiked === "true"
-        ? { connect: { id: parseInt(userId) } }
-        : { disconnect: { id: parseInt(userId) } };
+        ? { connect: { id: req.user.id } }
+        : { disconnect: { id: req.user.id } };
   }
 
   if (isBookmarked) {
     action.bookmarkedBy =
       isBookmarked === "true"
-        ? { connect: { id: parseInt(userId) } }
-        : { disconnect: { id: parseInt(userId) } };
+        ? { connect: { id: req.user.id } }
+        : { disconnect: { id: req.user.id } };
   }
 
   const post = await prisma.post.update({
@@ -113,6 +121,37 @@ const getPostById = async (req, res) => {
     where: {
       id: parseInt(postId),
     },
+    include: {
+      likedBy: {
+        select: {
+          id: true,
+        },
+      },
+      bookmarkedBy: {
+        select: {
+          id: true,
+        },
+      },
+      comments: {
+        select: {
+          id: true,
+        },
+      },
+
+      author: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          profile: {
+            select: {
+              imageUrl: true,
+            },
+          },
+        },
+      },
+    },
+
   });
 
   if (!post) return res.sendStatus(500);
